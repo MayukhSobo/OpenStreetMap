840a51a69f47843e90b99fbdbe568c85f6b65183
MayukhSobo <mayukh2012@hotmail.com>
2017-04-11 00:49:15 +0530

    Started adding the export dataset

--- src/dataFrame/DataFramerXML.py
+++ src/dataFrame/DataFramerXML.py
@@ -1,5 +1,6 @@
 import xml.etree.cElementTree as ET
 from abc import ABC, abstractmethod
+import os
 
 
 class ParserXML(ABC):
@@ -23,9 +24,11 @@ class ParserXML(ABC):
 		if self.root.tag.lower() != 'osm':
 			raise ValueError('OpenStreetDataMap Error')
 
-	@abstractmethod
-	def parse(self):
-		pass
+	def _get_element(self, tags):
+		for event, elem in self.context:
+			if event == 'end' and elem.tag in self.tags:
+				yield elem
+				self.root.clear()
 
 	@abstractmethod
 	def export_dataset(fraction):
@@ -35,17 +38,19 @@ class ParserXML(ABC):
 class DataFramerXML(ParserXML):
 	def __init__(self, osmFile, tags, **kwargs):
 		super(self.__class__, self).__init__(osmFile=osmFile)
+		self._parseData = ""
 		self.tags = tags
-		self.exportMaps = kwargs.get('files')
+		self.exportFiles = kwargs.get('files')
 		self.export = kwargs.get('export')
 		self.tagValidation = kwargs.get('tagValidation', False)
+		self.rootPath = kwargs.get('root', '.')
 		self._validate()
 
 	def _validate(self):
 		if not self.export:
 			pass
-		elif self.export is None and self.exportMaps is not None or self.export is not None and self.exportMaps is None:
-			raise ValueError('Export value and exportMaps doesn\'t match')
+		elif self.export is None and self.exportFiles is not None or self.export is not None and self.exportFiles is None:
+			raise ValueError('Export value and exportFiles doesn\'t match')
 
 		if self.tagValidation:
 			allTags = set()
@@ -57,15 +62,24 @@ class DataFramerXML(ParserXML):
 				if each not in allTags:
 					raise ValueError('Given tags are not present in XML')
 
-	def parse(self):
-		pass
+	@property
+	def data(self):
+		self.export_dataset()
+		return self._parseData
 
 	def export_dataset(self):
-		pass
+		if not os.path.exists(self.rootPath):
+			raise IOError('The path doesn\'t exist.')
+		for exportFile, factor in self.exportFiles.items():
+			if exportFile.split('.')[-1] != 'osm':
+				raise ValueError('Export files must OSM type')
+			print(exportFile, factor)
 
 
 def main():
-	DataFramerXML('../../gurugram.osm', ['q'])
+	p = DataFramerXML('../../gurugram.osm', tags=('node', 'way', 'relation'), files={'test.osm': 10})
+	# p.parse(factor=10000)
+	p.data
 
 
 if __name__ == '__main__':
--- test/test.py
+++ test/test.py
@@ -1,17 +1,35 @@
-import xml.etree.cElementTree as ET
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-osm_file = '../gurugram.osm'
+import xml.etree.ElementTree as ET  # Use cElementTree or lxml if too slow
 
-context = iter(ET.iterparse(osm_file, events=('start', 'end')))
-_, root = next(context)
-tags = ('node', 'way', 'relation')
-allTags = set()
-for event, elem in context:
-	allTags.add(elem.tag)
+OSM_FILE = "../gurugram.osm"  # Replace this with your osm file
+SAMPLE_FILE = "sample.osm"
 
-for each in tags:
-	if each not in allTags:
-		print('OOPs!!')
-# 	if elem.tag == 'way':
-# 		print(elem.tag)
-# print(root.tag)
+k = 10000  # Parameter: take every k-th top level element
+
+
+def get_element(osm_file, tags=('node', 'way', 'relation')):
+    """Yield element if it is the right type of tag
+
+    Reference:
+    http://stackoverflow.com/questions/3095434/inserting-newlines-in-xml-file-generated-via-xml-etree-elementtree-in-python
+    """
+    context = iter(ET.iterparse(osm_file, events=('start', 'end')))
+    _, root = next(context)
+    for event, elem in context:
+        if event == 'end' and elem.tag in tags:
+            yield elem
+            root.clear()
+
+
+with open(SAMPLE_FILE, 'wb') as output:
+    output.write(b'<?xml version="1.0" encoding="UTF-8"?>\n')
+    output.write(b'<osm>\n  ')
+
+    # Write every kth top level element
+    for i, element in enumerate(get_element(OSM_FILE)):
+        if i % k == 0:
+            output.write(ET.tostring(element, encoding='utf-8'))
+
+    output.write(b'</osm>')
