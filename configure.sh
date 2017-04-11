8ffa63b8364598f7c1de50c3ad40a24e4a6107dd
MayukhSobo <mayukh2012@hotmail.com>
2017-04-11 20:14:36 +0530

    Added the configure.sh to automate raw file decompression

--- configure.sh
+++ configure.sh
@@ -1,6 +1,18 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 if [ -d "raw_data" ]; then
 	# unpack the osm compressed file
-	bzip2 -d "raw_data/gurugram.osm.bz2"
+	FILE_PATH=raw_data/*
+	for eachRawFile in $FILE_PATH
+	do
+		bzip2 -dk $eachRawFile
+		IN=$eachRawFile
+		set -- "$IN"
+		IFS="/"; declare -a Array=($*)
+		_FILE=${Array[1]}
+		set -- "$_FILE"
+		IFS="."; declare -a Array=($*)
+		mv raw_data/${Array[0]}.${Array[1]} res/
+		IFS=""
+	done
 fi
