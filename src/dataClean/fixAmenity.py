440a1a581d6da17ed352ddb58b500017664e5993
MayukhSobo <mayukh2012@hotmail.com>
2017-04-22 20:28:48 +0530

    Improved Documentation

--- src/dataClean/fixAmenity.py
+++ src/dataClean/fixAmenity.py
@@ -57,7 +57,7 @@ def fixAtms(data):
 	We need to make it uniform because later on we
 	may perform a groupby operations on all the atms
 	to find which operator has most atms.
-	:param: data - restructured and verified node data and way data
+	:param: data - restructured and verified node data
 	"""
 	searchMatchValue = 'atm'
 	for each in data:
@@ -74,10 +74,10 @@ def fixAtms(data):
 def extractAtms(data):
 	"""
 	It is extracting all the atms from the
-	banks. It is taking a node/way having both
+	banks. It is taking a node having both
 	'atm' and 'bank' and creating two separate
-	nodes/ways with two separate 'amenities'
-	:param: data - restructured and verified node data and way data
+	nodes with two separate 'amenities'
+	:param: data - restructured and verified node data
 	"""
 	searchMatchValue = 'bank'
 	for each in data:
@@ -95,26 +95,41 @@ def extractAtms(data):
 
 
 def NameUnify(data, amenity):
+	"""
+	This make all the bank or atm names unified.
+	This cleaning was important because same banks
+	were included with different names which could
+	have created problems during later stage. This
+	works only on 'bank' and 'atm'. Recommended to
+	use only after 'fixAtms()' and 'extractAtms()'
+
+	:param: amenity - can be either 'bank' or 'atm'
+	:param: data - restructured and verified node data
+	"""
+	if amenity not in ['bank', 'atm']:
+		raise NotImplementedError("NameUnify can not unity nodes/ways with {}".format(amenity))
 	searchMatchValue = amenity
 	for each in data:
 		if searchMatchTag in each['k'] and searchMatchValue in each['v'] and each.get('removed') != 'true':
 			tagValueData = dict(zip(each['k'], each['v']))
-			bankName = tagValueData.get('name')
-			hdfc = len(re.findall(r'([hH][dD][fF][cC])', bankName)) >= 1
-			icici = len(re.findall(r'([iI][cC][iI][cC][iI])', bankName)) >= 1
-			axis = len(re.findall(r'([aA][xX][iI][sS])', bankName)) >= 1
-			citi = len(re.findall(r'([cC][iI][tT][iI])', bankName)) >= 1
-			sbi = len(re.findall(r'([sS][bB][iI]|[sS][tT][aA][tT][eE])', bankName)) >= 1
-			kotak = len(re.findall(r'([kK][oO][tT][aA][kK])', bankName)) >= 1
-			hsbc = len(re.findall(r'[hH][sS][bB][cC]', bankName)) >= 1
-			pnb = len(re.findall(r'([pP][nN][bB]|[pP][uU][nN][jJ][aA][bB])', bankName)) >= 1
-			obc = len(re.findall(r'([Oo][bB][cC]|[Oo][rR][iI][eE][nN][tT][aA][Ll])', bankName)) >= 1
-			boi = len(re.findall(r'([bB][oO][iI]|[iI][nN][dD][iI][aA])', bankName)) >= 1
-			indusland = len(re.findall(r'([iI][nN][dD][uU][sS])', bankName)) >= 1
-			scb = len(re.findall(r'([Cc][hH][aA][rR][tT][eE][rR])', bankName)) >= 1
+			amenityName = tagValueData.get('name')
+			# ### Some most common bank/atm operator names ####
+			hdfc = len(re.findall(r'([hH][dD][fF][cC])', amenityName)) >= 1
+			icici = len(re.findall(r'([iI][cC][iI][cC][iI])', amenityName)) >= 1
+			axis = len(re.findall(r'([aA][xX][iI][sS])', amenityName)) >= 1
+			citi = len(re.findall(r'([cC][iI][tT][iI])', amenityName)) >= 1
+			sbi = len(re.findall(r'([sS][bB][iI]|[sS][tT][aA][tT][eE])', amenityName)) >= 1
+			kotak = len(re.findall(r'([kK][oO][tT][aA][kK])', amenityName)) >= 1
+			hsbc = len(re.findall(r'[hH][sS][bB][cC]', amenityName)) >= 1
+			pnb = len(re.findall(r'([pP][nN][bB]|[pP][uU][nN][jJ][aA][bB])', amenityName)) >= 1
+			obc = len(re.findall(r'([Oo][bB][cC]|[Oo][rR][iI][eE][nN][tT][aA][Ll])', amenityName)) >= 1
+			boi = len(re.findall(r'([bB][oO][iI]|[iI][nN][dD][iI][aA])', amenityName)) >= 1
+			indusland = len(re.findall(r'([iI][nN][dD][uU][sS])', amenityName)) >= 1
+			scb = len(re.findall(r'([Cc][hH][aA][rR][tT][eE][rR])', amenityName)) >= 1
 			banks = [(hdfc, 'HDFC Bank'), (icici, 'ICICI Bank'), (axis, 'AXIS Bank'), (citi, 'CITI Bank'), (sbi, 'State Bank of India'), (kotak, 'KOTAK MAHINDRA'), (hsbc, 'HSBC Bank'), (pnb, 'Punjab National Bank'), (obc, 'ORIENTAL BANK OF COMMERCE'), (boi, 'BANK OF INDIA'), (indusland, 'INDUSLAND BANK'), (scb, 'Standard Chartated Bank')]
 			for bank in banks:
 				if bank[0]:
+					# Because dicts are passed by reference
 					temp = deepcopy(each)
 					temp['v'][temp['k'].index('name')] = bank[1]
 					# #### For most common bank and atms ####
