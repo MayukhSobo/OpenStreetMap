80373cc7225e9c8e5f63abc1e56185e12aa03a6e
MayukhSobo <mayukh2012@hotmail.com>
2017-05-01 01:01:46 +0530

    Added the configure.sh

--- configure.sh
+++ configure.sh
@@ -1,29 +1,3 @@
 #!/bin/sh
-
-if [ -d "res" ]; then
-    rm -rf res
-fi
-
-if [ ! -d "res" ]; then
-    mkdir res
-fi
-
-
-if [ -d "raw_data" ]; then
-	# unpack the osm compressed file
-	FILE_PATH=raw_data/*
-	for eachRawFile in $FILE_PATH
-	do
-		bzip2 -dk $eachRawFile
-		IN=$eachRawFile
-		set -- "$IN"
-		IFS="/"; declare -a Array=($*)
-		_FILE=${Array[1]}
-		set -- "$_FILE"
-		IFS="."; declare -a Array=($*)
-		mv raw_data/${Array[0]}.${Array[1]} res/
-		IFS=""
-	done
-fi
-
-python "src/main.py" "gurugram.osm" 
+pip install -q virtualenv
+virtualenv venv --python=python3.6 --no-site-packages -q
