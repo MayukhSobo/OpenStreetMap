72277f488f601c84160a4e42121da3bc1c4118b2
Mayukh Sarkar <mayukh2012@hotmail.com>
2017-05-02 17:54:23 +0530

    Report plots

--- test/test.py
+++ test/test.py
@@ -1,13 +1,32 @@
-import json
-from pprint import pprint
+#!/usr/bin/env python
 
 
-# with open('test.json', 'w') as json_file:
-# 	json_file.write('{\n')
-# 	json_file.write('	"name": "Mayukh"')
-# 	json_file.write('\n}')
+def get_db(db_name):
+    from pymongo import MongoClient
+    client = MongoClient('localhost:27017')
+    db = client[db_name]
+    return db
 
-with open('../export/export_way.json') as json_file:
-	data = iter(json.load(json_file))
 
-pprint(data)
+def make_pipeline():
+    # complete the aggregation pipeline
+    match1 = {"$match": {"amenity": {"$exists": 1, "$in": ["atm", "bank"]}}}
+    group = {"$group": {"_id": "$name", "total": {"$sum": 1}}}
+    sort = {"$sort": {"total": -1}}
+    project = {"$project": {"_id": 0, "bank_or_atm": "$_id", "count": "$total"}}
+    pipeline = [match1, group, sort, project]
+    return pipeline
+
+
+def aggregate(db, pipeline):
+    return [doc for doc in db.openStreetDataMap.aggregate(pipeline)]
+
+
+if __name__ == '__main__':
+    db = get_db('udacity')
+    pipeline = make_pipeline()
+    result = aggregate(db, pipeline)
+    # print(type(result))
+    # assert len(result) == 1
+    import pprint
+    pprint.pprint(result)
